c4abd8d7e496d8bbbfec26bb5d57ca6dd3d1513f
Xujie <six@gatech.edu>
2017-01-17 14:47:07 -0500

    add maxsat command stub

---
+++ main/src/nichrome/maxsat/incremental/MaxSATCommandLine.java
@@ -0,0 +1,15 @@
+package nichrome.maxsat.incremental;
+
+import nichrome.util.AbstractBaseCommand;
+import nichrome.util.CommandInterface;
+
+public class MaxSATCommandLine extends AbstractBaseCommand implements CommandInterface {
+
+	@Override
+	public boolean execute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+}
