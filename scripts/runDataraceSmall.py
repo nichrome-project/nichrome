610fcdb907f8f6d315ed2c3fd1ab9e2ff5b75ee6
Xin Zhang <xin@huntsman-ve565-2422.apn.wlan.upenn.edu>
2017-07-06 21:51:10 -0400

    update

--- scripts/runDataraceSmall.py
+++ scripts/runDataraceSmall.py
@@ -9,7 +9,7 @@ settings=['bagged', 'oracle', 'dynamic', 'staticAggr', 'staticCons']
 
 for bench in benches:
     for setting in settings:
-        cmd = 'python ./ursaDatarace.py '+bench + ' ' +setting + + ' '+code
+        cmd = 'python ./ursaDatarace.py '+bench + ' ' +setting +' '+code
         print 'Running '+cmd
         process = subprocess.Popen(cmd, shell=True) 
         process.wait()
@@ -19,7 +19,7 @@ bench = 'raytracer'
 
 setting = 'uniform'
 
-cmd = 'python ./ursaDatarace.py '+bench + ' ' +setting + + ' '+code
+cmd = 'python ./ursaDatarace.py '+bench + ' ' +setting + ' '+code
 print 'Running '+cmd
 process = subprocess.Popen(cmd, shell=True) 
 process.wait()
