02945910a0bea24813900624c824ea47c274d869
Xin Zhang <xin@huntsman-ve565-2422.apn.wlan.upenn.edu>
2017-07-07 16:17:28 -0400

    update

--- plotting/extractData.py
+++ plotting/extractData.py
@@ -108,8 +108,8 @@ def extractData(client, benchmarks, configs):
                     nfqs = int(tokens[1])
                     if len(mainData[bench]) == 0:
                         mainData[bench].append(nfqs)
-                        if mainData[bench] <= 1:
-                            mainData[bench] = nfqs
+                    elif mainData[bench][0] <= 1:
+                        mainData[bench][0] = nfqs
                 i+=1
                 # end iterating log file
             ifs.close()
